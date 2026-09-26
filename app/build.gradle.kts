9899f5236e106088287b2828e51682b95f5a9911
Gz319 <itguazige@gmail.com>
2026-09-27 00:26:46 +0800

    sync: 图隐套件源码同步到远端

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.setgo.tank"
         minSdk = 33
         targetSdk = 37
-        versionCode = 51
-        versionName = "v1.0.2"
+        versionCode = 53
+        versionName = "v1.0.3"
     }
 
     buildFeatures {
